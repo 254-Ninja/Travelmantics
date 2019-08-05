1bbe1497f9d45ad565775cd56d252178c9aaf2c9
perfect-code-ninja <muthomidenis62@gmail.com>
2019-08-05 23:12:56 +0300

    firebase

--- app/src/main/java/com/example/travelmantics/DealActivity.java
+++ app/src/main/java/com/example/travelmantics/DealActivity.java
@@ -1,105 +1,216 @@
 package com.example.travelmantics;
 
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
 
 public class DealActivity extends AppCompatActivity {
-    private FirebaseDatabase mFirebaseDatabase;
+    public static final int PICTURE_RESULT = 42;
     private DatabaseReference mDatabaseReference;
+    private FirebaseDatabase mFirebaseDatabase;
     EditText txtTitle;
-    EditText txtDescription;
     EditText txtPrice;
+    EditText txtDescription;
+    ImageView imageView;
     TravelDeal deal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        FirebaseUtil.openFbReference("traveldeals");
-        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
-        mDatabaseReference = FirebaseUtil.mDatabaseReference;
-        txtTitle = (EditText) findViewById(R.id.txtTitle);
-        txtDescription = (EditText) findViewById(R.id.txtDescription);
-        txtPrice= (EditText) findViewById(R.id.txtPrice);
+        setContentView(R.layout.activity_deal);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+
+        txtTitle = findViewById(R.id.txtTitle);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtPrice = findViewById(R.id.txtPrice);
+        imageView = findViewById(R.id.imageDeal);
+
         Intent intent = getIntent();
-        TravelDeal deal= (TravelDeal) intent.getSerializableExtra("Deal");
+        deal = (TravelDeal) intent.getSerializableExtra("Deal");
         if (deal==null){
-            deal= new TravelDeal();
+            deal = new TravelDeal();
         }
-        this.deal = deal;
-        txtTitle.setText(deal.getTitle());
+//        this.deal = deal;
         txtDescription.setText(deal.getDescription());
         txtPrice.setText(deal.getPrice());
-
+        txtTitle.setText(deal.getTitle());
+        showImage(deal.getImageUrl());
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
             case R.id.save_menu:
                 saveDeal();
-                Toast.makeText(this, "Deal saved", Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"Deal saved", Toast.LENGTH_LONG).show();
                 clean();
                 backToList();
                 return true;
             case R.id.delete_menu:
                 deleteDeal();
-                Toast.makeText(this, "Deal Deleted", Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"Deal deleted", Toast.LENGTH_LONG).show();
                 backToList();
                 return true;
-                default:
-                    return super.onOptionsItemSelected(item);
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.save_menu, menu);
+        MenuItem saveMenu = menu.findItem(R.id.save_menu);
+        MenuItem deleteMenu = menu.findItem(R.id.delete_menu);
+        Button imageButton = findViewById(R.id.btnImage);
+        if(FirebaseUtil.isAdmin){
+            saveMenu.setVisible(true);
+            deleteMenu.setVisible(true);
+            enableEditTexts(true);
+            imageButton.setEnabled(true);
+        }
+        else{
+            saveMenu.setVisible(false);
+            deleteMenu.setVisible(false);
+            enableEditTexts(false);
+            imageButton.setEnabled(false);
+        }
         return true;
     }
-    private void saveDeal(){
+
+    private void clean() {
+        txtPrice.setText("");
+        txtDescription.setText("");
+        txtTitle.setText("");
+        txtTitle.requestFocus();
+    }
+
+    private void saveDeal() {
         deal.setTitle(txtTitle.getText().toString());
         deal.setDescription(txtDescription.getText().toString());
         deal.setPrice(txtPrice.getText().toString());
-        if(deal.getId()==null) {
+        if (deal.getId()==null){
             mDatabaseReference.push().setValue(deal);
         }
         else {
             mDatabaseReference.child(deal.getId()).setValue(deal);
         }
-
     }
 
     private void deleteDeal(){
-        if (deal == null) {
-            Toast.makeText(this,"please save the deal before deleting", Toast.LENGTH_SHORT).show();
+        if (deal.getId()==null){
+            Toast.makeText(this, "Please save the deal before attempting to delete", Toast.LENGTH_SHORT).show();
             return;
         }
         mDatabaseReference.child(deal.getId()).removeValue();
+        Log.d("Image deleting", "image "+deal.getImageName());
+        if(deal.getImageName()!=null && !deal.getImageName().isEmpty()){
+            StorageReference picReference = FirebaseUtil.mFirebaseStorage.getReference().child(deal.getImageName());
+            picReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Log.d("Delete Image", "onSuccess: Image succesfully deleted");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("Delete Image", "onFailure: "+e.getMessage());
+                }
+            });
+        }
     }
 
     private void backToList(){
         Intent intent = new Intent(this, ListActivity.class);
         startActivity(intent);
     }
-    private void clean(){
-        txtTitle.setText("");
-        txtPrice.setText("");
-        txtDescription.setText("");
-        txtTitle.requestFocus();
+
+    private void enableEditTexts(boolean isEnabled){
+        txtTitle.setEnabled(isEnabled);
+        txtDescription.setEnabled(isEnabled);
+        txtPrice.setEnabled(isEnabled);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==PICTURE_RESULT && resultCode==RESULT_OK){
+            assert data != null;
+            final Uri imageUri = data.getData();
+            final StorageReference
+                    reference = FirebaseUtil.mStorageReference.child(imageUri.getLastPathSegment());
+            UploadTask uploadTask = reference.putFile(imageUri);
+            Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return reference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        assert downloadUri != null;
+                        String imageUrl = downloadUri.toString();
+                        String imageName = task.getResult().getLastPathSegment();
+                        Log.d("imageUrl", "onSuccess: "+downloadUri.toString());
+                        deal.setImageUrl(imageUrl);
+                        deal.setImageName(imageName);
+                        showImage(imageUrl);
+                    } else {
+                        Toast.makeText(DealActivity.this, "Picture couldn't be uploaded", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+    }
+
+    public void editImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/jpeg");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+        startActivityForResult(Intent.createChooser(intent, "Insert Picture"), PICTURE_RESULT);
+    }
+
+    public void showImage(String url){
+        if(url!=null && !url.isEmpty()){
+            int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+            Picasso.get()
+                    .load(url)
+                    .resize(width, width*2/3)
+                    .centerCrop()
+                    .into(imageView);
+        }
     }
 }
-
